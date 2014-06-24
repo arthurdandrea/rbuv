ead111a8e26f98570cfdb9b3b849e5fac3ca7e7b
Arthur D'Andrea <aalemmar@gmail.com>
2014-06-24 18:11:37 -0300

    Add singleton methods to access libuv version

--- ext/rbuv/rbuv.c
+++ ext/rbuv/rbuv.c
@@ -4,10 +4,16 @@ ID id_call;
 
 VALUE mRbuv;
 
+VALUE rbuv_version(VALUE self);
+VALUE rbuv_version_string(VALUE self);
+
 void Init_rbuv() {
   id_call = rb_intern("call");
 
   mRbuv = rb_define_module("Rbuv");
+  rb_define_singleton_method(mRbuv, "version", rbuv_version, 0);
+  rb_define_singleton_method(mRbuv, "version_string", rbuv_version_string, 0);
+
   Init_rbuv_error();
   Init_rbuv_handle();
   Init_rbuv_loop();
@@ -15,4 +21,12 @@ void Init_rbuv() {
   Init_rbuv_stream();
   Init_rbuv_tcp();
   Init_rbuv_signal();
+}
+
+VALUE rbuv_version(VALUE self) {
+  return UINT2NUM(uv_version());
+}
+
+VALUE rbuv_version_string(VALUE self) {
+  return rb_str_new2(uv_version_string());
 }
