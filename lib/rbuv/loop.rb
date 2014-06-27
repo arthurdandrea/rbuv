0501cf71a81841b91296953d03f4d51e704abc3e
Arthur D'Andrea <aalemmar@gmail.com>
2014-06-27 16:30:19 -0300

    Wrap all Rbuv::Loop methods in ruby to receive a block and create a one shot timer

--- lib/rbuv/loop.rb
+++ lib/rbuv/loop.rb
@@ -12,30 +12,35 @@ module Rbuv
       Signal.new(self)
     end
 
-    def run_with_block(&block)
-      one_shot_timer(0, &block) if block
-      run_without_block
+    alias_method :c_run, :run
+    private :c_run
+    def run(&block)
+      Timer.one_shot(self) { yield self } if block_given?
+      c_run
     end
-    alias_method :run_without_block, :run
-    alias_method :run, :run_with_block
+
+    alias_method :c_run_once, :run_once
+    private :c_run_once
+    def run_once(&block)
+      Timer.one_shot(self) { yield self } if block_given?
+      c_run_once
+    end
+
+    alias_method :c_run_nowait, :run_nowait
+    private :c_run_nowait
+    def run_nowait(&block)
+      Timer.one_shot(self) { yield self } if block_given?
+      c_run_nowait
+    end
 
     def dispose
-      return if self.handles.empty?
+      return self if self.handles.empty?
       self.handles.each do |handle|
         handle.close unless handle.closing?
       end
       run
       raise RuntimeError unless self.handles.empty?
-    end
-
-    private
-
-    def one_shot_timer(timeout, &block)
-      timer = Timer.new(self)
-      timer.start(timeout, 0) do
-        timer.close
-        block.call
-      end
+      self
     end
   end
 end
--- lib/rbuv/timer.rb
+++ lib/rbuv/timer.rb
@@ -1,5 +1,14 @@
 module Rbuv
   class Timer
+    def self.one_shot(loop=nil, &block)
+      timer = new(loop)
+      timer.start(0, 0) do
+        timer.close
+        block.call
+      end
+      nil
+    end
+
     def self.start(*args, &block)
       if args.first.is_a? Rbuv::Loop
         timer = new(args.shift)
