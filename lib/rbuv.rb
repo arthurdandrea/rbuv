58acd761f8e878b56549a39915bc0328bd1e835d
Arthur D'Andrea <aalemmar@gmail.com>
2014-07-04 14:25:29 -0300

    Add documentation for module Rbuv

--- lib/rbuv.rb
+++ lib/rbuv.rb
@@ -7,22 +7,33 @@ require 'rbuv/loop'
 module Rbuv
   class << self
 
-    def run_loop
-      Loop.default.run
-    end
-
+    # Stop the default loop.
+    # @see Rbuv::Loop#stop
+    # @return [Rbuv::Loop] the {Rbuv::Loop.default}
     def stop_loop
       Loop.default.stop
     end
 
     alias stop stop_loop
 
-    def run(&block)
+    # Run the default loop.
+    # @see Rbuv::Loop#run
+    # @yield (see Rbuv::Loop#run)
+    # @yieldparam loop [Rbuv::Loop] the {Rbuv::Loop.default}
+    # @return [Rbuv::Loop] the {Rbuv::Loop.default}
+    def run_loop(&block)
       Loop.default.run(&block)
     end
 
-    def run_block
-      Loop.default.run_once { yield }
+    alias run run_loop
+
+    # Run the default loop once.
+    # @see Rbuv::Loop#run_once
+    # @yield (see Rbuv::Loop#run_once)
+    # @yieldparam loop [Rbuv::Loop] the {Rbuv::Loop.default}
+    # @return [Rbuv::Loop] the {Rbuv::Loop.default}
+    def run_block(&block)
+      Loop.default.run_once(&block)
     end
 
   end
