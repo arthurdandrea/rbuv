c8788749ded6879dacf27e7260ace153c31f4152
Arthur D'Andréa Alemar <aalemmar@gmail.com>
2015-03-12 18:12:45 -0300

    on travis, use libuv 0.10.36

--- install-libuv.sh
+++ install-libuv.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -ex
-LIBUV=1.4.2
+LIBUV=0.10.36
 wget https://github.com/libuv/libuv/archive/v${LIBUV}.tar.gz --output-document libuv-${LIBUV}.tar.gz
 tar -xf libuv-${LIBUV}.tar.gz
 mv libuv-${LIBUV} libuv
 cd libuv
-sh autogen.sh
-./configure
 make
