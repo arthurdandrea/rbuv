e7734743ea930fcdbf0e379daa1c4946ab6d805f
Arthur D'Andréa Alemar <aalemmar@gmail.com>
2015-03-13 00:20:13 -0300

    uncommenting major instructions on install-libuv.sh

--- install-libuv.sh
+++ install-libuv.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -ex
 LIBUV=1.4.2
-# wget https://github.com/libuv/libuv/archive/v${LIBUV}.tar.gz --output-document libuv-${LIBUV}.tar.gz
-# tar -xf libuv-${LIBUV}.tar.gz
-# mv libuv-${LIBUV} libuv
+wget https://github.com/libuv/libuv/archive/v${LIBUV}.tar.gz --output-document libuv-${LIBUV}.tar.gz
+tar -xf libuv-${LIBUV}.tar.gz
+mv libuv-${LIBUV} libuv
 cd libuv
 sh autogen.sh
 ./configure
