47109738f6a72546b1cf453cc582e4a1fce0a159
Arthur D'Andréa Alemar <aalemmar@gmail.com>
2015-08-10 12:59:14 -0300

    fix travis build by compiling libuv with flag "-fPIC"

--- install-libuv.sh
+++ install-libuv.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -ex
 LIBUV=1.7.0
+export CFLAGS="-fPIC"
 wget https://github.com/libuv/libuv/archive/v${LIBUV}.tar.gz --output-document libuv-${LIBUV}.tar.gz
 tar -xf libuv-${LIBUV}.tar.gz
 mv libuv-${LIBUV} libuv
