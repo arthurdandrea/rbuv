5a2f354cf6909ea515bec3a43c6811e6dc12d7c4
Arthur D'Andréa Alemar <aalemmar@gmail.com>
2015-08-19 10:34:10 -0300

    Simplify extconf.rb

--- ext/rbuv/extconf.rb
+++ ext/rbuv/extconf.rb
@@ -6,12 +6,6 @@ if have_library('uv', 'uv_run', ['uv.h'])
   have_header('ruby/thread.h')
   have_func('rb_thread_call_without_gvl', 'ruby/thread.h')
 
-  case RbConfig::CONFIG["host_os"]
-  when /darwin/
-    have_framework("CoreFoundation")
-    have_framework("CoreServices") if RUBY_ENGINE =~ /rbx/
-  end
-
   ##
   # Adds -DRBUV_DEBUG for compilation
   # To turn it on, use: --with-debug or --enable-debug
@@ -19,11 +13,6 @@ if have_library('uv', 'uv_run', ['uv.h'])
   if debug_arg = with_config("debug") || enable_config("debug")
     debug_def = "-DRBUV_DEBUG"
     $defs.push(debug_arg.is_a?(String) ? "#{debug_def}=#{debug_arg}" : debug_def) unless $defs.include?(/\A#{debug_arg}/)
-  end
-
-  case RUBY_ENGINE
-  when /rbx/
-    $defs.push("-DRBUV_RBX") unless $defs.include? "-DRBUV_RBX"
   end
 
   create_header
